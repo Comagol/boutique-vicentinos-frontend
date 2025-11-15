1b1b698ccb6ffa86bd9fc461ac5d2922a7bdbd13
Comagol <comabellafrancisco@gmail.com>
2025-11-15 19:40:15 -0300

    Add AdminLayout component to define the structure for admin pages, utilizing Flex and Box from Chakra UI for layout management.

---
+++ frontend/src/app/layouts/AdminLayout.tsx
@@ -0,0 +1,12 @@
+import { Flex, Box } from "@chakra-ui/react";
+import { Outlet } from "react-router-dom";
+
+export const AdminLayout = () => {
+  return (
+    <Flex bg="background.dim" minH="100vh">
+      <Box flex= "1" p={8}>
+        <Outlet />
+      </Box>
+    </Flex>
+  )
+}
