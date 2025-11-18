081e3cc45a0b6b380408ed6c11d9fd71b59336b1
Comagol <comabellafrancisco@gmail.com>
2025-11-17 23:24:40 -0300

    Integrate Footer component into PublicLayout to provide site-wide information, improving overall layout structure and user experience.

--- frontend/src/app/layouts/PublicLayout.tsx
+++ frontend/src/app/layouts/PublicLayout.tsx
@@ -1,13 +1,16 @@
 import { Box } from "@chakra-ui/react"
 import { Outlet } from "react-router-dom"
 import { Navbar } from "../../components/Navbar"
+import { Footer } from "../../components/Footer"
 
 export const PublicLayout = () => {
   return (
     <Box bg="background.surface" minH="100vh">
       <Navbar />
-      <Outlet />
-      {/* TODO: FOOTER */}
+      <Box flex="1">
+        <Outlet />
+      </Box>
+      <Footer />
     </Box>
   )
 }
