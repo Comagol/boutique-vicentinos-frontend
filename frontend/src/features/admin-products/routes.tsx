df2520dd89b522dc97968c6de06b9af80efed381
Comagol <comabellafrancisco@gmail.com>
2025-11-25 00:01:55 -0300

    Update AdminProductsRoutes to include CreateProductPage and EditProductPage components, enhancing product management capabilities in the admin interface.

--- frontend/src/features/admin-products/routes.tsx
+++ frontend/src/features/admin-products/routes.tsx
@@ -1,9 +1,12 @@
 import { Route } from "react-router-dom";
 import { AdminProductsPage } from "./pages/AdminProductsPage";
+import { CreateProductPage } from "./pages/CreateProductPage";
+import { EditProductPage } from "./pages/EditProductPage";
 
 export const AdminProductsRoutes = (
   <>
     <Route index element={<AdminProductsPage />} />
-    <Route path="products/:productId" element={<AdminProductsPage />} />
+    <Route path="products/new" element={<CreateProductPage />} />
+    <Route path="products/:productId" element={<EditProductPage />} />
   </>
 );
